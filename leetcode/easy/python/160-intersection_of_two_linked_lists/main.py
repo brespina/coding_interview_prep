6eae17be2ba6c4adbdaf8a815dc9c8ae732143ca
brespina <brespina4@gmail.com>
2025-03-18 03:14:48 -0500

    intersecting two linked lists

--- leetcode/easy/python/160-intersection_of_two_linked_lists/main.py
+++ leetcode/easy/python/160-intersection_of_two_linked_lists/main.py
@@ -1,16 +1,40 @@
 """
     160. Intersection of two linked lists
 """
+from typing import Optional
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 def get_intersection_node(head_a: ListNode, head_b: ListNode) -> Optional[ListNode]:
+    stackA = ['A']
+    stackB = ['A']
+
+    while head_a or head_b:
+        if head_a:
+            stackA.append(head_a)
+            head_a = head_a.next
+        if head_b:
+            stackB.append(head_b)
+            head_b = head_b.next
+        
+        prev = None
+        while stackA and stackB:
+            nodeA = stackA.pop(-1)
+            nodeB = stackB.pop(-1)
+
+            if nodeA != nodeB:
+                return prev
+            
+            prev = nodeA
+    
 
 
 
 def main():
+    print("writing test case too complicated for me rn.")
 
 
 if __name__ == "__main__":
