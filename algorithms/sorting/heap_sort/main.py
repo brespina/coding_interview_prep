0cad12634bf7f7b1934b0cbe9e24c6640f5234f0
brespina <brespina4@gmail.com>
2025-03-19 18:04:27 -0500

    heap sort was in merge sort directory

---
+++ algorithms/sorting/heap_sort/main.py
@@ -0,0 +1,31 @@
+"""
+    Heap sort
+
+time complexity
+    Best               Average            Worst
+Omega(n log(n))  |  Theta(n log(n))  |  O(n log(n))
+
+space complexity
+Worst
+O(1)
+"""
+
+
+from typing import List
+
+
+def heapify(arr: List[int], n: int, i: int) -> None:
+
+    
+    
+def heap_sort(arr: List[int]) -> None:
+    
+    
+    
+def main():
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
